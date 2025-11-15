059d0e225fbbdf162ff99fe5dc61d92b28a4742d
vicadlsn <cadinspect@gmail.com>
2025-11-15 21:51:34 +0300

    fix: timestamps type

--- migrations/000001_init_schema.up.sql
+++ migrations/000001_init_schema.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS teams (
     team_name VARCHAR(64) PRIMARY KEY,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS users (
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(64) NOT NULL,
     team_name VARCHAR(64) NOT NULL REFERENCES teams(team_name) ON UPDATE CASCADE,
     is_active BOOLEAN NOT NULL DEFAULT TRUE,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX IF NOT EXISTS idx_users_team_name ON users(team_name);
@@ -20,8 +20,8 @@ CREATE TABLE IF NOT EXISTS pull_requests (
     pull_request_name VARCHAR(64) NOT NULL,
     author_id VARCHAR(64) NOT NULL REFERENCES users(user_id),
     status VARCHAR(6) NOT NULL CHECK (status IN ('OPEN', 'MERGED')),
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    merged_at TIMESTAMP NULL
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    merged_at TIMESTAMP WITH TIME ZONE NULL
 );
 
 CREATE INDEX IF NOT EXISTS idx_pr_status ON pull_requests(status);
@@ -30,7 +30,7 @@ CREATE INDEX IF NOT EXISTS idx_pr_author_id ON pull_requests(author_id);
 CREATE TABLE IF NOT EXISTS pr_reviewers (
     pull_request_id VARCHAR(64) NOT NULL REFERENCES pull_requests(pull_request_id) ON DELETE CASCADE,
     user_id VARCHAR(64) NOT NULL REFERENCES users(user_id),
-    assigned_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    assigned_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     
     PRIMARY KEY (pull_request_id, user_id)
 );
